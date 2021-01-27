2591e41906372c2a49463c0152431f249fef931d
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-01-26 23:48:28 -0300

    Aula 19

--- funcoes/callback_3.js
+++ funcoes/callback_3.js
@@ -8,3 +8,18 @@ const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
 const primeiraLetra = texto => texto[0]
 const letras = nomes.map(primeiraLetra)
 console.log(nomes, letras)
+
+const carrinho = [
+    { nome: 'Caneta', qtde: 10, preco: 7.99 },
+    { nome: 'Impressora', qtde: 0, preco: 649.50 },
+    { nome: 'Caderno', qtde: 4, preco: 27.10 },
+    { nome: 'Lapis', qtde: 3, preco: 5.82 },
+    { nome: 'Tesoura', qtde: 1, preco: 19.20 }
+]
+
+const getNome = item => item.nome
+console.log(carrinho.map(getNome))
+
+const getTotal = item => item.qtde * item.preco
+const totais = carrinho.map(getTotal)
+console.log(totais)
