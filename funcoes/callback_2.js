321da1fc81f280303d0ac024e5c0e00047ea9e40
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-01-26 23:29:45 -0300

    Aula 16

---
+++ funcoes/callback_2.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+const caminho = path.join(__dirname, 'dados.txt')
+
+function exibirConteudo(_, conteudo) {
+    console.log(conteudo.toString())
+}
+
+console.log('Início')
+fs.readFile(caminho, exibirConteudo)
+fs.readFile(caminho, (_, conteudo) => console.log(conteudo.toString()))
+console.log('Fim')
+
+console.log('Inicio Sync')
+const conteudo = fs.readFileSync(caminho)
+console.log(conteudo.toString())
+console.log('Fim Sync')
