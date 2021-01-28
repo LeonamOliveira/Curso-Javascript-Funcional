a6421dce7fa0bd9e97127ba63a201281de037148
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-01-27 21:59:28 -0300

    Aula 23

--- funcoes/callback_4.js
+++ funcoes/callback_4.js
@@ -16,3 +16,19 @@ const nomesItensValido = carrinho
     .map(getNome)
 
 console.log(nomesItensValido)
+
+Array.prototype.meuFilter = function(fn) {
+    const novoArray = []
+    for(let i = 0; i < this.length; i++) {
+        if(fn(this[i], i, this)){
+            novoArray.push(this[i])
+        }
+    }
+    return novoArray
+}
+
+const nomesItensValido2 = carrinho
+    .meuFilter(qtdeMaiorQueZero)
+    .map(getNome)
+
+console.log(nomesItensValido2)
