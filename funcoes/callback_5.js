d74e945a73612819c474c479805d5e0e5bac52dd
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-01-28 20:41:42 -0300

    Aula 25

---
+++ funcoes/callback_5.js
@@ -0,0 +1,16 @@
+const carrinho = [
+    { nome: 'Caneta', qtde: 10, preco: 7.99 },
+    { nome: 'Impressora', qtde: 0, preco: 649.50 },
+    { nome: 'Caderno', qtde: 4, preco: 27.10 },
+    { nome: 'Lapis', qtde: 3, preco: 5.82 },
+    { nome: 'Tesoura', qtde: 1, preco: 19.20 }
+]
+
+const getTotal = item => item.qtde * item.preco
+const somar = (acc, el) => acc + el
+
+const totalGeral = carrinho
+    .map(getTotal)
+    .reduce(somar, 0)
+
+console.log(totalGeral)
