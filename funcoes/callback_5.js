bfd3336760a5a8f82ad306628c6856edda201206
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-01-29 00:08:08 -0300

    Aula 28

--- funcoes/callback_5.js
+++ funcoes/callback_5.js
@@ -14,3 +14,21 @@ const totalGeral = carrinho
     .reduce(somar, 0)
 
 console.log(totalGeral)
+
+Array.prototype.meuReduce = function (fn, inicial) {
+    let acc = inicial
+    for (let i = 0; i < this.length; i++) {
+        if (!acc & i === 0) {
+            acc = this[i]
+            continue
+        }
+        acc = fn(acc, this[i], i, this)
+    }
+    return acc
+}
+
+const totalGeral2 = carrinho
+    .map(getTotal)
+    .reduce(somar, 0)
+
+console.log(totalGeral2)
