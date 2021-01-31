81c871cc344566dc7ccf3fc78b7aa0e00b6ddb41
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-01-31 12:04:45 -0300

    Aula 33

---
+++ funcoes/desafio_3.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const { join } = require('path')
+const path = require('path')
+
+function lerArquivo(caminho) {
+    return new Promise(resolve => {
+        fs.readFile(caminho, function (_, conteudo) {
+            resolve(conteudo.toString())
+        })
+        console.log('Depois de ler')
+    })
+}
+
+const caminho = path.join(__dirname, 'dados.txt')
+lerArquivo(caminho)
+    .then(conteudo => conteudo.split('\n'))
+    .then(linhas => linhas.join(','))
+    .then(conteudo => `O valor final é: ${conteudo}`)
+    .then(console.log)
