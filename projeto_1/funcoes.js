e1779fa2c5b05b121a9a3493c9332c7af4fa1ade
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-02-02 17:49:15 -0300

    Aula 47

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -1,8 +1,16 @@
 const fs = require('fs')
 const path = require('path')
+
 function lerDiretorio(caminho) {
-    let arquivos = fs.readdirSync(caminho)
-    return arquivos.map(arquivo => path.join(caminho, arquivo))
+    return new Promise((resolve, reject) => {
+        try {
+            let arquivos = fs.readdirSync(caminho)
+            arquivos = arquivos.map(arquivo => path.join(caminho, arquivo))
+            resolve(arquivos)
+        } catch (e) {
+            reject(e)
+        }
+    })
 }
 
 module.exports = {
--- projeto_1/index.js
+++ projeto_1/index.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const { log } = require('util')
 const fn = require('./funcoes')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
-const arquivos = fn.lerDiretorio(caminho)
-console.log(arquivos)
+fn.lerDiretorio(caminho)
+
+    .then(console.log)
