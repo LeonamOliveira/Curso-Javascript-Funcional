20835366d0d24368ea9730acad133fe3bb0e3fb6
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-02-05 11:00:21 -0300

    Aula 55

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -28,23 +28,39 @@ function lerArquivos(caminhos) {
     return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
 }
 
-function elementosTerminadosCom(array, padraoTextual) {
-    return array.filter(el => el.endsWith(padraoTextual))
+function elementosTerminadosCom(padraoTextual) {
+    return function(array) {
+        return array.filter(el => el.endsWith(padraoTextual))
+    }
 }
 
-function removerSeVazio(array) {
+function removerElementosSeVazio(array) {
     return array.filter(el => el.trim())
 }
 
-function removerSeIncluir(array, padraoTextual) {
-    return array.filter(el => !el.includes(padraoTextual))
+function removerElementosSeIncluir(padraoTextual) {
+    return function(array) {
+        return array.filter(el => !el.includes(padraoTextual))
+    }
 }
 
-function removerSeApenasNumero(array) {
+function removerElementosSeApenasNumero(array) {
     return array.filter(el => {
         const num = parseInt(el.trim())
         return num !== num
     })
+}
+
+function removerSimbolos(simbolos) {
+    return function(array) {
+        return array.map(el => {
+            let textoSemSimbolos = el
+            simbolos.forEach(simbolo => {
+                textoSemSimbolos = textoSemSimbolos.split(simbolo).join(' ')
+            })
+            return textoSemSimbolos
+        })
+    }
 }
 
 module.exports = {
@@ -52,7 +68,8 @@ module.exports = {
     lerArquivo,
     lerArquivos,
     elementosTerminadosCom,
-    removerSeVazio,
-    removerSeIncluir,
-    removerSeApenasNumero
+    removerElementosSeVazio,
+    removerElementosSeIncluir,
+    removerElementosSeApenasNumero,
+    removerSimbolos
 }
--- projeto_1/index.js
+++ projeto_1/index.js
@@ -1,15 +1,16 @@
 const path = require('path')
-const { log } = require('util')
 const fn = require('./funcoes')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
+
 fn.lerDiretorio(caminho)
-    .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
-    .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
+    .then(fn.elementosTerminadosCom('.srt'))
+    .then(fn.lerArquivos)
     .then(conteudos => conteudos.join('\n'))
     .then(todoConteudo => todoConteudo.split('\n'))
-    .then(linhas => fn.removerSeVazio(linhas))
-    .then(linhas => fn.removerSeIncluir(linhas, '-->'))
-    .then(linhas => fn.removerSeApenasNumero(linhas))
+    .then(fn.removerElementosSeVazio)
+    .then(fn.removerElementosSeIncluir('-->'))
+    .then(fn.removerElementosSeApenasNumero)
+    .then(fn.removerSimbolos(simbolos))
     .then(console.log)
