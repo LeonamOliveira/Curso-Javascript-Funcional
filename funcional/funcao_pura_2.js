404ca1cddb5ba6a697d89bba4d8c7f030650cd52
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-02-07 01:38:50 -0300

    Aula 64

---
+++ funcional/funcao_pura_2.js
@@ -0,0 +1,12 @@
+
+function gerarNumeroAleatorio(min, max) {
+    const fator = max - min + 1
+    return parseInt(Math.random() * fator) + min
+}
+
+console.log(gerarNumeroAleatorio(1, 10000))
+console.log(gerarNumeroAleatorio(1, 10000))
+console.log(gerarNumeroAleatorio(1, 10000))
+console.log(gerarNumeroAleatorio(1, 10000))
+console.log(gerarNumeroAleatorio(1, 10000))
+console.log(gerarNumeroAleatorio(1, 10000))
