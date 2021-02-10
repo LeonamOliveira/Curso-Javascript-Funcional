5e1763fdc955f1701334e8769948d23b44a38ccb
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-02-09 22:15:37 -0300

    Aula 68

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -4,11 +4,10 @@ const path = require('path')
 function lerDiretorio(caminho) {
     return new Promise((resolve, reject) => {
         try {
-            const arquivos = fs.readdirSync(caminho)
-            const arquivosCompletos = arquivos.map(arquivo => {
+            const arquivos = fs.readdirSync(caminho).map(arquivo => {
                 return path.join(caminho, arquivo)
             })
-            resolve(arquivosCompletos)
+            resolve(arquivos)
         } catch (e) {
             reject(e)
         }
@@ -86,7 +85,7 @@ function ordenarPorAtribNumerico(attr, ordem = 'asc') {
     return function (array) {
         const asc = (o1, o2) => o1[attr] - o2[attr]
         const desc = (o1, o2) => o2[attr] - o1[attr]
-        return array.sort(ordem === 'asc' ? asc : desc)
+        return [...array].sort(ordem === 'asc' ? asc : desc)
     }
 }
 
