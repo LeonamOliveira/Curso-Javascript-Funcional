8d8e10de745f2b9cf214c91d50b18834746deb2c
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-02-14 18:47:53 -0300

    Aula 77

---
+++ funcional/composicao_2.js
@@ -0,0 +1,46 @@
+
+function composicao(...fns) {
+    return function (valor) {
+        return fns.reduce(async (acc, fn) => {
+            if (Promise.resolve(acc) === acc) {
+                return fn(await acc)
+            } else {
+                return fn(acc)
+            }
+        }, valor)
+    }
+}
+
+function gritar(texto) {
+    return texto.toUpperCase()
+}
+
+function enfatizar(texto) {
+    return `${texto} !!!`
+}
+
+function tornarLento(texto) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(texto.split('').join(' '))
+        }, 3000)
+    })
+}
+
+const exagerado = composicao(
+    gritar,
+    enfatizar,
+    tornarLento
+)
+
+
+const umPoucoMenosExagerado = composicao(
+    gritar,
+    enfatizar
+)
+
+exagerado('PARA')
+    .then(console.log)
+
+    umPoucoMenosExagerado('cuidado com o buraco')
+    .then(console.log)
