0c4c6a7d854bf28f2c65798d218a78c2f38e784a
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-02-28 13:27:15 -0300

    Aula 91

---
+++ rxjs/observable_1.js
@@ -0,0 +1,22 @@
+const { Observable } = require('rxjs')
+
+const promise = new Promise(resolve => {
+    resolve('Promise é bem legal!')
+    resolve('Promise é bem legal!')
+})
+
+promise.then(console.log)
+
+const obs = new Observable(subscriber => {
+    subscriber.next('Observer!')
+    subscriber.next('é')
+    subscriber.next('bem')
+    setTimeout(() => {
+        subscriber.next('legal!')
+        subscriber.complete()
+        console.log('Completo!')
+    }, 1000)
+})
+
+obs.subscribe(console.log)
+obs.subscribe(texto => console.log(texto.toUpperCase()))
