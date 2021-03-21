d2de4544c4f31f4822f4c05c754c26adc9c22567
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-03-21 00:37:38 -0300

    Aula 99

---
+++ rxjs/scheduler.js
@@ -0,0 +1,9 @@
+const { from, asyncScheduler } = require('rxjs')
+const { observeOn } = require('rxjs/operators')
+
+console.log('Início')
+
+from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    .pipe(observeOn(asyncScheduler))
+    .subscribe(console.log)
+console.log('Fim')
