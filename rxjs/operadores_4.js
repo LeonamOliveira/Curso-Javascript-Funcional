5bf8515bdf9ca9c1148633a476ae39ca874fa773
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-03-26 21:15:43 -0300

    Aula 102

--- rxjs/operadores_4.js
+++ rxjs/operadores_4.js
@@ -16,9 +16,28 @@ function primeiro() {
 function ultimo() {
     return function(source) {
         return Observable.create(subscriber => {
+            let ultimo
             source.subscribe({
                 next(v) {
-                    subscriber.next(v + 2000)
+                    ultimo = v
+                },
+                complete() {
+                    if(ultimo !== undefined) {
+                        subscriber.next(ultimo)
+                    }
+                    subscriber.complete()
+                }
+            })
+        })
+    }
+}
+
+function nenhum() {
+    return function(source) {
+        return Observable.create(subscriber => {
+            source.subscribe({
+                next(v) {
+                    subscriber.complete()
                 }
             })
         })
@@ -26,7 +45,10 @@ function ultimo() {
 }
 
 from([1, 2, 3, 4, 5])
-    // .pipe(primeiro())
-    .pipe(ultimo())
+    .pipe(
+        // primeiro(),
+        // nenhum(),        
+        ultimo()
+        )
     .subscribe(console.log)
 
