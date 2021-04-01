00c7b8f170387fd6a6ba85c39c1fe5df5b1dacae
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-03-31 22:43:15 -0300

    Aula 104

---
+++ rxjs/operadores_5.js
@@ -0,0 +1,29 @@
+const { of, Obersvable, Observable } = require('rxjs')
+
+function terminadoCom(parteFinal) {
+    return function (fonte) {
+        return Observable.create(subscriber => {
+            fonte.subscribe({
+                next(valor) {
+                    if (Array.isArray(valor)) {
+                        subscriber.next(
+                            valor.filter(el => el.endsWith(parteFinal))
+                        )
+                    } else if (valor.endsWith(parteFinal)) {
+                        subscriber.next(valor)
+                    }
+                },
+                error(e) {
+                    subscriber.error(e)
+                },
+                complete() {
+                    subscriber.complete()
+                }
+            })
+        })
+    }
+}
+
+of(['Ana Silva', 'Maria Silva', 'Pedro Rocha'])
+    .pipe(terminadoCom('Silva'))
+    .subscribe(console.log)
