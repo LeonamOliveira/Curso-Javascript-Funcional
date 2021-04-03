cd36304573a5ea7c5ef18483f3506758ae4317c4
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-04-03 00:41:13 -0300

    Aula 108

--- rxjs/operadores_6.js
+++ rxjs/operadores_6.js
@@ -1,58 +1,55 @@
 const { from, Observable } = require('rxjs')
 
-function createPipeableOperator(nextGenerator) { 
-    return function(source) {
+function createPipeableOperator(operatorFn) {
+    return function (source) {
         return Observable.create(subscriber => {
+            const sub = operatorFn(subscriber)
             source.subscribe({
-                next: nextGenerator(subscriber)
+                next: sub.next,
+                error: sub.error || (e => subscriber.error(e)),
+                complete: sub.complete || (() => subscriber.complete())
             })
         })
     }
 }
 
 function primeiro() {
-    return createPipeableOperator(subscriber => v => {
-                subscriber.next(valor)
-                subscriber.complete()
-    })
+    return createPipeableOperator(subscriber => ({
+        next(v) {
+            subscriber.next(v)
+            subscriber.complete()
+        }
+    }))
 }
 
 function ultimo() {
-    return function(source) {
-        return Observable.create(subscriber => {
-            let ultimo
-            source.subscribe({
-                next(v) {
-                    ultimo = v
-                },
-                complete() {
-                    if(ultimo !== undefined) {
-                        subscriber.next(ultimo)
-                    }
-                    subscriber.complete()
-                }
-            })
-        })
-    }
+    let ultimo
+    return createPipeableOperator(subscriber => ({
+        next(v) {
+            ultimo = v
+        },
+        complete() {
+            if(ultimo !== undefined) {
+                subscriber.next(ultimo)
+            }
+            subscriber.complete()
+        }
+    }))
 }
 
 function nenhum() {
-    return function(source) {
-        return Observable.create(subscriber => {
-            source.subscribe({
-                next(v) {
-                    subscriber.complete()
-                }
-            })
-        })
-    }
+    return createPipeableOperator(subscriber => ({
+        next(v) {
+            subscriber.complete()
+        }
+    }))
 }
 
 from([1, 2, 3, 4, 5])
     .pipe(
-        primeiro(),
-        // nenhum(),        
-        // ultimo()
-        )
+        // primeiro(),
+        // nenhum(),
+        ultimo()
+    )
     .subscribe(console.log)
 
