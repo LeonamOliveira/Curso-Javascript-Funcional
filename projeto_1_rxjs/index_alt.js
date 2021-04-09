e93a65f925bb759be7ee87b36fe8a88388f3f54e
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-04-09 18:21:23 -0300

    Aula 124

--- projeto_1_rxjs/index_alt.js
+++ projeto_1_rxjs/index_alt.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fn = require('./funcoes')
 const _ = require('lodash')
-const { toArray, map } = require('rxjs/operators')
+const { toArray, map, groupBy, reduce, mergeMap } = require('rxjs/operators')
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
 const simbolos = [
@@ -21,8 +21,10 @@ fn.lerDiretorio(caminho)
         fn.removerSimbolos(simbolos),
         fn.removerElementosSeVazio(),
         fn.removerElementosSeApenasNumero(),
+        groupBy(el => el),
+        mergeMap(grupo => grupo.pipe(toArray())),
+        map(palavras => ({elemento: palavras[0], qtde: palavras.length})),
         toArray(),
-        fn.agruparElementos(),
         map(array => _.sortBy(array, el => -el.qtde))
     )
     .subscribe(console.log)
